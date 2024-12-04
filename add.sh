14230a6b885eba434ecee32df86342db2343873e
Herman Streltsov <mail@streltsov.dev>
2024-12-04 08:06:29 +0100

    Update

--- add.sh
+++ add.sh
@@ -2,17 +2,22 @@
 
 source config.sh
 
+while true; do
 read -e -r -p "Front: " front
-read -e -r -p "Back: " back
-
-record="0"
-record+=$DELIMETER
-record+=$(date +"%Y-%m-%d %H:%M:%S")
-record+=$DELIMETER
-record+=$front
-record+=$DELIMETER
-record+=$back
-
-[ -e "$DECK_PATH" ] || touch "$DECK_PATH"
-
-echo "$record" >> "$DECK_PATH"
+  read -e -r -p "Back: " back
+  
+  record="0"
+  record+=$DELIMETER
+  # TODO: Add first interval
+  record+=$(date +"%Y-%m-%d %H:%M:%S")
+  record+=$DELIMETER
+  record+=$front
+  record+=$DELIMETER
+  record+=$back
+  
+  DECK_DIR_PATH=$(dirname "$DECK_PATH")
+  [ -d "$DECK_DIR_PATH" ] || mkdir -p "$DECK_DIR_PATH"
+  [ -e "$DECK_PATH" ] || touch "$DECK_PATH"
+  
+  echo "$record" >> "$DECK_PATH"
+done
--- config.sh
+++ config.sh
@@ -1,4 +1,4 @@
 export DELIMETER="|"
-export DECK_PATH="$HOME/.shellcards.csv"
+export DECK_PATH="$HOME/.shellcards/deck.csv"
 export INTERVALS=(120 600 3600 18000 86400 432000 1036800 2160000 10368000)
 
