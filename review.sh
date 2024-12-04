fc40a7aa8c1ae686c44b25893809e229c4f15f26
Herman Streltsov <mail@streltsov.dev>
2024-12-04 22:28:41 +0100

    update

--- review.sh
+++ review.sh
@@ -1,66 +1,47 @@
 #!/bin/bash
 
 source config.sh
+
+ask() {
+  while true; do
+    read -e -r -p "$1 (Y/n): " response
+    response=${response,,}
+    [[ -z "$response" || "$response" == "y" || "$response" == "n" ]] && break
+  done
+
+    [[ -z "$response" || "$response" == "y" ]] && return 0
+    [[ "$response" == "n" ]] && return 0
+}
+
 # TODO: Skip records which are in TOP BOX
-# TODO: Rename record to card
-# TODO: Polish texts
 
 [ -e "$DECK_PATH" ] || touch "$DECK_PATH"
 
 ready_to_review=""
 
-# Find cards ready to review
-while IFS= read -r record; do
-  IFS="|" read -r _box date _front _back  <<< "$record"
-  review_date_seconds=$(date -d "$date" +%s 2>/dev/null)
-  current_date_seconds=$(date +%s)
+while IFS= read -r card; do
+  IFS="|" read -r _box date _front _back  <<< "$card"
 
-  if (( review_date_seconds < current_date_seconds )); then
-    ready_to_review+="$record"
-    ready_to_review+=$'\n'
+  if (( $(date -d "$date" +%s) < $(date +%s) )); then
+    ready_to_review+="$card"$'\n'
   fi
 done < "$DECK_PATH"
 
 # TODO: Fix this IFS reassigning
 IFS=$'\n'
-for record in $ready_to_review; do
-  IFS="|" read -r box date front back  <<< "$record"
+for card in $ready_to_review; do
+  IFS="|" read -r box date front back  <<< "$card"
 
-    echo "$front"
+  echo -e "\n  [ $front ]\n"
+  read -r -p "Hit return to show back side"
+  echo -e "\033[4A\n  [ $front ] [ $back ]\n"
 
-    while true; do
-      read -r -e -p "Do you remember this word(y/n)?" answer
+  updated_box=$(ask "Did you remember it?" && echo $((box + 1)) || echo 0)
+  updated_date_seconds=$(($(date +%s) + INTERVALS[updated_box]))
+  updated_date=$(date -d "@$updated_date_seconds" +"%Y-%m-%d %H:%M:%S")
+  updated_card="$updated_box|$updated_date|$front|$back"
+  sed -i "s/${card}/${updated_card}/g" "$DECK_PATH"
 
-      if [ "$answer" == y ]; then
-        updated_box=$((box + 1))
-        _current_date_seconds=$(date +%s)
-        updated_date_seconds=$((_current_date_seconds + INTERVALS[updated_box]))
-        updated_date=$(date -d "@$updated_date_seconds" +"%Y-%m-%d %H:%M:%S")
-        updated_record="$updated_box|$updated_date|$front|$back"
-        sed -i "s/${record}/${updated_record}/g" "$DECK_PATH"
-        echo "$back"
-        break
-      fi
-
-      if [ "$answer" == n ]; then
-        updated_box=0
-        _current_date_seconds=$(date +%s)
-        updated_date_seconds=$((_current_date_seconds + INTERVALS[updated_box]))
-        updated_date=$(date -d "@$updated_date_seconds" +"%Y-%m-%d %H:%M:%S")
-        updated_record="$updated_box|$updated_date|$front|$back"
-        sed -i "s/${record}/${updated_record}/g" "$DECK_PATH"
-        echo "$back"
-        break
-      fi
-    done
-
-
-    # TODO: Proceed with enter
-    while true; do
-      read -r -e -p "Proceed? (Y)" answer
-      if [ y == "$answer" ]; then
-        break
-      fi
-    done
-IFS=$'\n'
+  echo -e "\033[3A"
+  IFS=$'\n'
 done
