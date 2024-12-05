b0e9b26a68efcc6386b55d80cf2d9165490bcc14
Herman Streltsov <mail@streltsov.dev>
2024-12-05 17:21:04 +0100

    Update

--- add.sh
+++ add.sh
@@ -1,23 +1,21 @@
 #!/bin/bash
 
+set -euo pipefail
 source config.sh
+DATE_FORMAT="+%Y-%m-%d %H:%M:%S"
 
 while true; do
-read -e -r -p "Front: " front
+  read -e -r -p "Front: " front
   read -e -r -p "Back: " back
-  
-  record="0"
-  record+=$DELIMETER
-  # TODO: Add first interval
-  record+=$(date +"%Y-%m-%d %H:%M:%S")
-  record+=$DELIMETER
-  record+=$front
-  record+=$DELIMETER
-  record+=$back
-  
+
+  box=0
+  seconds=$(($(date +%s) + ${INTERVALS[box]}))
+  date=$(date -d "@$seconds" "$DATE_FORMAT")
+  record="0$DELIMETER$date$DELIMETER$front$DELIMETER$back"
+
   DECK_DIR_PATH=$(dirname "$DECK_PATH")
   [ -d "$DECK_DIR_PATH" ] || mkdir -p "$DECK_DIR_PATH"
   [ -e "$DECK_PATH" ] || touch "$DECK_PATH"
-  
-  echo "$record" >> "$DECK_PATH"
+
+  echo "$record" >>"$DECK_PATH"
 done
--- review.sh
+++ review.sh
@@ -1,38 +1,37 @@
 #!/bin/bash
-
+set -euo pipefail
 source config.sh
 
 ask() {
   while true; do
-    read -e -r -p "$1 (Y/n): " response < /dev/tty
+    read -e -r -p "$1 (Y/n): " response </dev/tty
     response=${response,,}
     [[ -z "$response" || "$response" == "y" || "$response" == "n" ]] && break
   done
 
   [[ -z "$response" || "$response" == "y" ]] && return 0 || return 1
 }
-# TODO: move_up() { tput cuu "$1"; }
-
-[ -e "$DECK_PATH" ] || touch "$DECK_PATH"
 
 ready_to_review=""
 
 while IFS= read -r card; do
-  IFS="|" read -r _box date _front _back  <<< "$card"
+  IFS="|" read -r box date _front _back <<<"$card"
   [[ "$box" == "${#INTERVALS[@]}" ]] && continue
   [[ $(date -d "$date" +%s) -lt $(date +%s) ]] && ready_to_review+="$card"$'\n'
-done < "$DECK_PATH"
+done <"$DECK_PATH"
 
-while IFS="|" read -r box date front back; do
+echo "$ready_to_review" | while IFS= read -r card; do
+  IFS="|" read -r box date front back <<<"$card"
+
   echo -e "\n  [ $front ]\n"
-  read -r -p "Hit return to show back side" < /dev/tty
+  read -r -p "Hit return to show back side" </dev/tty
   echo -e "\033[4A\n  [ $front ] [ $back ]\n"
 
   updated_box=$(ask "Did you remember it?" && echo $((box + 1)) || echo 0)
   updated_date_seconds=$(($(date +%s) + INTERVALS[updated_box]))
   updated_date=$(date -d "@$updated_date_seconds" +"%Y-%m-%d %H:%M:%S")
-  updated_card="$updated_box|$updated_date|$front|$back"
+  updated_card="$updated_box$DELIMETER$updated_date$DELIMETER$front$DELIMETER$back"
   sed -i "s/${card}/${updated_card}/g" "$DECK_PATH"
 
   echo -e "\033[3A"
-done <<< "$ready_to_review"
+done
